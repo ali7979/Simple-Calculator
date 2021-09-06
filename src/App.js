2b318b46887f6dbb9a7061fd107ea7010365c69c
Zoheb Alam <zohebzob@gmail.com>
2021-09-06 22:03:32 +0530

    simple

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+const [exp,setexp] = useState("")
+const ops=['/','*','+','-']
+var cln=()=>{
+  setexp([])
+}
+
+ 
+var fn=(e)=>{
+console.log(e.target.innerText);
+var value=e.target.innerText
+setexp(exp+value)
+if(value=="=")
+{
+  var res=eval(exp)
+  console.log("ali")
+  console.log(res)
+  setexp(`RESULT IS :  ${res}`)
+}
+
+
+}
+
+
+var bck=()=>{
+  setexp(exp.slice(0, -1))
+}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main">
+    
+      <div className='box'>
+
+        <div className="Title"> Calculator</div>
+        <div className="row">{exp}
+      <button className="backspc" onClick={bck}><i class="fas fa-angle-double-left"></i></button>
+ </div>
+ <div className="btnrow">
+   <button onClick={cln} className="grey">AC</button>
+   <button onClick={fn} className="grey">+/-</button>
+   <button onClick={fn} className="grey">%</button>
+   <button onClick={fn} className="btnq">/</button>
+ 
+ 
+ 
+ 
+ </div>
+
+ <div className="btnrow">
+   <button onClick={fn} className="btn">1</button>
+   <button onClick={fn} className="btn">2</button>
+   <button onClick={fn} className="btn">3</button>
+   <button onClick={fn} className="btnq">*</button>
+ </div>
+
+ <div className="btnrow">
+   <button  onClick={fn} className="btn">4</button>
+   <button onClick={fn}  className="btn">5</button>
+   <button  onClick={fn} className="btn">6</button>
+   <button onClick={fn}  className="btnq">-</button>
+ 
+ </div>
+
+
+ <div className="btnrow">
+   <button  onClick={fn}  className="btn">7</button>
+   <button  onClick={fn} className="btn">8</button>
+   <button  onClick={fn} className="btn">9</button>
+   <button onClick={fn}  className="btnq">+</button>
+ 
+ </div>
+
+ <div className="btnrow">
+   
+   <button  onClick={fn} className="btnz">0</button>
+   <button onClick={fn}  className="btn">.</button>
+   <button onClick={fn}  className="btnq">=</button>
+ 
+ </div>
+
+      </div>
     </div>
+    </>
   );
 }
 
